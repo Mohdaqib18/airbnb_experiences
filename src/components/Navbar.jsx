e7bc09826023473a438df1cc1526a367024edcd8
Mohd Aqib <mohdaqib18@gmail.com>
2022-10-16 00:13:15 +0530

    airBnb app created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import logo from "../../public/assets/airbnb.png";
+
+export default function Navbar(){
+    return(
+        <nav>
+            <img src={logo} className="nav--logo"/>
+        </nav>
+    )
+}
